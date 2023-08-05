ad8ecf5d61878fac472629e11f1a5cb0e0d8ad7f
toqvist <tobias-ahlqvist@outlook.com>
2023-08-05 09:36:47 +0200

    Automatically fetch anki decks

--- src/renderer/src/modes/Analyze.tsx
+++ src/renderer/src/modes/Analyze.tsx
@@ -1,7 +1,7 @@
 import FilePreview from '../components/FilePreview'
 import FileActions from '../components/FileActions'
 import { useFileContext } from '../contexts/FileContext'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function Process(): JSX.Element {
@@ -12,11 +12,13 @@ function Process(): JSX.Element {
 
   const [decks, setDecks] = useState<Deck[]>([])
 
-  async function getDecks () {
-    console.log("get decks")
-    console.log(await window.api.getDecks())
-    setDecks(await window.api.getDecks())  
+  async function getDecks() {
+    setDecks(await window.api.getDecks())
   }
+
+  useEffect(() => {
+    getDecks();
+  }, []);
 
   return (
     <div className='previews'>
@@ -25,15 +27,24 @@ function Process(): JSX.Element {
         <p>Clicking word brings up all sentences that uses that word on the right</p>
       </div>
       <div>
-        <button onClick={() => getDecks()}>get decks</button>
-        {decks.length == 0
-          ? <i>No anki decks found</i>
-          : <select>
-            {decks.map((deck) => (
-              <option value={deck.name}>{deck.name}</option>
-            ))}
-          </select>
-        }
+        <div>
+          {/* TODO: Force user to pick a deck if they want to add a cloze, i.e. disable cloze button if no deck is picked*/}
+          <span>Deck: </span> 
+          {decks.length == 0
+            ? <>
+              <i>No anki decks found</i>
+              <button onClick={() => getDecks()}>Retry</button>
+              <div>
+                <p>Make sure anki is running with <a href="https://ankiweb.net/shared/info/2055492159">AnkiConnect</a> active</p>
+              </div>
+            </>
+            : <select>
+              {decks.map((deck) => (
+                <option value={deck.name}>{deck.name}</option>
+              ))}
+            </select>
+          }
+        </div>
         <h2>Used in sentences</h2>
         <div className="sentence"><p>I am an example <strong>sentence</strong></p><button>Cloze</button></div>
       </div>
