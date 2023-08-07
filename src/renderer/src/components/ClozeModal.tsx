79bc5376361ef98c528a377aa6f0a461857683e6
toqvist <tobias-ahlqvist@outlook.com>
2023-08-07 08:37:40 +0200

    Render cloze punctuation & spacing properly

--- src/renderer/src/components/ClozeModal.tsx
+++ src/renderer/src/components/ClozeModal.tsx
@@ -6,30 +6,69 @@ export interface ClozeModalProps {
 }
 
 
+interface WordObject {
+  text: string;
+  isPunctuation: boolean;
+  clozed: boolean;
+}
+
 function ClozeModal({ sentence }: ClozeModalProps): JSX.Element {
+  
+  function sentenceToString(sentence: Sentence): string {
+    return sentence.words.map((word) => word.text).join(' ');
+  }
 
-  const [words, setWords] = useState<Word[]>(sentence.words);
-  console.log(words)
+  //TODO: Merge WordObject/Word and their associated functions
+  function getWordObjects(sentence: Sentence): WordObject[] {
+    const sentenceText = sentenceToString(sentence);
+    const wordObjects: WordObject[] = [];
+    const wordPattern = /\w+/g;
+    const wordsAndPunctuation = sentenceText.match(
+      new RegExp(`${wordPattern.source}|\\s|[^\\w\\s]`, 'g')
+    );
 
-  function toggleClozed(wordToToggle: Word) {
-    setWords(prevWords => prevWords.map(word => {
-      if (word === wordToToggle) {
-        return { ...word, clozed: !word.clozed };
-      } else {
-        return word;
-      }
-    }));
+    if (wordsAndPunctuation) {
+      wordsAndPunctuation.forEach((text) => {
+        const isPunctuation = !wordPattern.test(text) && text.trim() !== '';
+        wordObjects.push({ text, isPunctuation, clozed: false });
+      });
+    }
+
+    return wordObjects;
+  }
+
+  const [wordObjects, setWordObjects] = useState<WordObject[]>(getWordObjects(sentence));
+
+  function toggleClozed(wordObjectToToggle: WordObject) {
+    setWordObjects((prevWordObjects) =>
+      prevWordObjects.map((wordObject) => {
+        if (wordObject === wordObjectToToggle) {
+          return { ...wordObject, clozed: !wordObject.clozed };
+        } else {
+          return wordObject;
+        }
+      })
+    );
   }
 
   return (
     <div className="modal-content">
       <div className="modal-card">
         <p>
-          {words.map((word, i) => {
-            if (word.clozed) {
-              return <a className="text-clozed" href="#" onClick={() => toggleClozed(word)}>{word.text + ' '}</a>
+          {wordObjects.map((wordObject, i) => {
+            if (!wordObject.isPunctuation) {
+              return (
+                <a
+                  key={i}
+                  className={wordObject.clozed ? 'text-clozed' : 'text-black'}
+                  href="#"
+                  onClick={() => toggleClozed(wordObject)}
+                >
+                  {wordObject.text}
+                </a>
+              );
             } else {
-              return <a className="text-black" href="#" onClick={() => toggleClozed(word)}>{word.text + ' '}</a>
+              return <span key={i}>{wordObject.text}</span>;
             }
           })}
         </p>
@@ -37,7 +76,7 @@ function ClozeModal({ sentence }: ClozeModalProps): JSX.Element {
         <i></i>
       </div>
     </div>
-  )
+  );
 }
 
 export default ClozeModal
