c53aa6d6e638380cbc711a31e40ac2d5341249b9
toqvist <tobias-ahlqvist@outlook.com>
2023-08-19 13:20:41 +0200

    Improve file upload styling

--- src/renderer/src/components/FileUpload.tsx
+++ src/renderer/src/components/FileUpload.tsx
@@ -18,18 +18,25 @@ function FileUpload(): JSX.Element {
   return (
     <div>
       {selectedFile.path === ''
-        ? <div className="upload">
+        ? <div className="upload border-2 border-slate-600 border-dotted ">
           <input
+            className="block w-full text-sm text-slate-400
+            file:mr-4 file:py-2 file:px-4
+            file:border-0
+            file:text-sm file:font-semibold
+          file:bg-slate-600 file:text-white
+           hover:file:bg-slate-500
+           "
             ref={fileInputRef}
             type="file"
             accept=".txt, .srt"
             onChange={handleFileChange}
           />
         </div>
-        : <div className='flex gap-4 items-center py-1 px-4 justify-between'>
+        : <div className='flex gap-4 items-center py-1 px-4 justify-between border-2 border-slate-600 border-dotted'>
           <p className='font-bold'>{selectedFile.name}</p>
           <button onClick={resetFile} className='bg-slate-700'>
-            <XMarkIcon className='h-6 w-6 stroke-slate-200'/>
+            <XMarkIcon className='h-6 w-6 stroke-slate-200' />
           </button>
         </div>
       }
